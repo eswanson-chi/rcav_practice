2b2d59e28459193586fc399c0e13793090b5952a
eswanson-chi <ericswanson@Erics-MacBook-Pro.local>
2015-07-29 01:06:11 -0500

    updated

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,5 +1,57 @@
 class CalculationsController < ApplicationController
+  def home
+    render("home.html.erb")
+  end
+
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+
+    @calc_one = params["calc_one"].to_i
+    @ans_one = @calc_one**2
+
+    render("square.html.erb")
+  end
+
+  def square_root
+
+    @calc_one = params["calc_one"].to_i
+    @ans_one = @calc_one**0.5
+
+    render("square_root.html.erb")
+  end
+
+  def random
+
+    calc_one = params["calc_one"].to_i
+    calc_two = params["calc_two"].to_i
+
+    @ans_one = rand(calc_one..calc_two)
+
+    render("random.html.erb")
+  end
+
+  def payment
+
+    apr = params["calc_one"].to_f
+    @apr = apr/100
+    @years = params["calc_two"].to_i
+    @principal = params["calc_three"].to_f
+
+    # A = Loan Amount
+    @a = @principal
+    # r = Rate of Interest (compounded) = APR/1200 [monthly]
+    @r = @apr / 1200
+    # N = Number of Payments = Number of Years * 12
+    @n = @years * 12
+    # P = Payment Amount
+    @payment = ( @r * @a ) / ( 1 - (1+@r)**-@n)
+
+    render("payment.html.erb")
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "instructions" })
+  get("/", { :controller => "calculations", :action => "home" })
+  get("/instructions", { :controller => "calculations", :action => "instructions" })
+  get("/square/:calc_one", { :controller => "calculations", :action => "square" })
+  get("/square_root/:calc_one", { :controller => "calculations", :action => "square_root" })
+  get("/random/:calc_one/:calc_two", { :controller => "calculations", :action => "random" })
+  get("/payment/:calc_one/:calc_two/:calc_three", { :controller => "calculations", :action => "payment" })
+
+
 end
